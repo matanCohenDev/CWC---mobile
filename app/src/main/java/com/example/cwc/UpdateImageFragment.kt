2954dacbcf122fad71ed17ccb7ed4e242529ad8d
matanCohenDev <matan209225457@cs.colman.ac.il>
2025-03-15 11:38:32 +0200

    add user

--- app/src/main/java/com/example/cwc/UpdateImageFragment.kt
+++ app/src/main/java/com/example/cwc/UpdateImageFragment.kt
@@ -179,6 +179,7 @@ class UpdateImageActivity : AppCompatActivity() {
       override fun onFailure(call: Call<CloudinaryUploadResponse>, t: Throwable) {
         onFailure("Upload failed: ${t.message}")
       }
+
     })
   }
 }
