8d8700dd419d18ea74a263753f44258562d92314
matanCohenDev <matan209225457@cs.colman.ac.il>
2025-03-23 18:34:36 +0200

    fix project

--- app/src/main/java/com/example/cwc/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/cwc/viewmodel/UserViewModel.kt
@@ -20,7 +20,6 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
   val user: LiveData<User?> get() = _user
 
   private val _logoutStatus = MutableLiveData<Boolean>()
-  val logoutStatus: LiveData<Boolean> get() = _logoutStatus
 
   fun addUser(user: User) {
     viewModelScope.launch {
@@ -35,41 +34,10 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
     }
   }
 
-  fun getUser(id: String) {
-    viewModelScope.launch {
-      val retrievedUser = repository.getUser(id)
-      _user.postValue(retrievedUser)
-    }
-  }
-
-  fun deleteUser(user: User) {
-    viewModelScope.launch {
-      repository.deleteUser(user)
-    }
-  }
-
   fun logout() {
     viewModelScope.launch {
       repository.logout()
       _logoutStatus.postValue(true)
-    }
-  }
-
-  // If imageBlob is null, load the image from profileImageUrl via Picasso
-  fun loadProfilePicture(userId: String, profileImage: ImageView) {
-    user.observeForever { user ->
-      user?.let {
-        if (it.imageBlob != null) {
-          val bitmap = BitmapFactory.decodeByteArray(it.imageBlob, 0, it.imageBlob.size)
-          profileImage.setImageBitmap(bitmap)
-        } else if (!it.profileImageUrl.isNullOrEmpty()) {
-          Picasso.get()
-            .load(it.profileImageUrl)
-            .placeholder(com.example.cwc.R.drawable.profile_foreground)
-            .error(com.example.cwc.R.drawable.profile_foreground)
-            .into(profileImage)
-        }
-      }
     }
   }
 }
--- app/src/main/java/com/example/cwc/viewmodel/UserViewModelFactory.kt
+++ app/src/main/java/com/example/cwc/viewmodel/UserViewModelFactory.kt
@@ -7,12 +7,10 @@ import com.example.cwc.data.repository.UserRepository
 class UserViewModelFactory(private val repository: UserRepository) : ViewModelProvider.Factory {
 
   override fun <T : ViewModel> create(modelClass: Class<T>): T {
-    // בדוק אם המחלקה הנדרשת היא UserViewModel
     if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
       return UserViewModel(repository) as T
     }
 
-    // זרוק חריגה מדויקת אם המחלקה אינה תואמת
     throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
   }
 }
